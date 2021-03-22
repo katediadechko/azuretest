5922560a64e8df81601cf71db794cd8f196f4576
katediadechko <78201351+katediadechko@users.noreply.github.com>
2021-03-22 11:27:41 +0100

    Add files via upload

---
+++ azuretest.py
@@ -0,0 +1,24 @@
+
+import json
+
+class Config:
+  def __init__(self, fname):
+    try:
+      f = open(fname, 'r')
+    except OSError:
+      print(f'Failed to open file {fname}')
+      return
+    with f:
+      try:
+        config = json.load(f)
+        self.uri = config['connection']['uri']
+        self.token = config['connection']['token']
+      except ValueError:
+        print(f'Failed to load json from {fname}')
+        return
+
+def main():
+  config = Config('azuretest.json')
+
+if __name__ == '__main__':
+  main()
