f9272d5b19f9b20fa4ec014f06b49693e4c7075f
katediadechko <78201351+katediadechko@users.noreply.github.com>
2021-03-23 11:23:03 +0100

    Parse URI to get collection and project

--- azuretest.py
+++ azuretest.py
@@ -11,16 +11,19 @@ class Config:
     with f:
       try:
         config = json.load(f)
-        self.uri = config['connection']['uri']
+        self.projectUri = config['connection']['projectUri']
         self.token = config['connection']['token']
-        self.project = config['connection']['project']
         self.testPlanId = config['connection']['testPlanId']
+        self.collection = self.projectUri.split('/')[-2]
+        self.project = self.projectUri.split('/')[-1]
       except ValueError:
         print(f'Failed to load json from {fname}')
         return
 
 def main():
   config = Config('azuretest.json')
+  print(config.collection)
+  print(config.project)
 
 if __name__ == '__main__':
   main()
